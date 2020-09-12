09c979bf202840f10729a8e1221b6eb23d1b153d
Archana Sugumar <64971959+archanaaaa@users.noreply.github.com>
2020-09-12 21:54:57 +0530

    Single Page Applications part 2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,15 +1,15 @@
 import React  from 'react'; 
-import { Card, CardImg , CardText, CardBody, CardTitle} from 'reactstrap';
+import { Card, CardImg , CardText, CardBody, CardTitle , Breadcrumb ,BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish})
     {
-        if(dish!=null)
-            {
+      
                 return(                 
-
-                    <div className="container"> 
-                        <div className="row">
-                            <div className="col-12 col-md-5 m-1"> 
+                    
+                    <div>   
+                        
+                            
                                 <Card>
                                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                                     <CardBody>
@@ -17,40 +17,37 @@ import { Card, CardImg , CardText, CardBody, CardTitle} from 'reactstrap';
                                             <CardText>{dish.description}</CardText>
                                     </CardBody> 
                                 </Card>
-                            </div>                    
-                            <div className="col-12 col-md-5 m-1"> 
-                                <RenderComments comments = {dish.comments} />
-                            </div>
-                            
-
-                        </div> 
+                                                                
+                        
                     </div>
-                )
-            }
-            else{
-                return(<div></div>);
-            }
+                );
+           
+          
 
         }
 
-        function RenderComments({comments})
+        function RenderComments({comment})
         {
-            const cmt=comments.map((comment)=>{
+            
+            const cmt=comment.map((comment)=>{
                 return(
                     
                         <div className="container">
+                            
                                     <div className="row">
                                         <p>{comment.comment}</p>
                                     </div>
                                     <div className="row">
                                         <p>--{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                                     </div>
-                                </div>               
+                                    
+                        </div>           
                 );
             });
-
+           
             return(
-            <div>
+            <div className="container">
+                
                 <Card>
                 <CardBody>
                     <CardTitle><h4>Comments</h4></CardTitle>
@@ -61,8 +58,10 @@ import { Card, CardImg , CardText, CardBody, CardTitle} from 'reactstrap';
                     </CardText>
                 </CardBody>
                 </Card>
-            </div>
+                </div>
+            
             );
+           
                 
             }
            
@@ -71,11 +70,43 @@ import { Card, CardImg , CardText, CardBody, CardTitle} from 'reactstrap';
     
 
     function DishDetail(props){
-        return(
-            <div> 
-           <RenderDish dish={props.dish}/>
-           </div>
-        );
+       
+            return(
+                <div className="container">
+                        <div className="row">
+                        <Breadcrumb>
+                        
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+    
+                            <BreadcrumbItem active>
+                                {props.dish.name}
+                            </BreadcrumbItem>
+    
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div> 
+                    <div className="container">
+                    <div class="row">
+                    <div class="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish}/>
+                    </div>
+                    <div class="col-12 col-md-5 m-1">
+                    <RenderComments comment = {props.comment}/>
+                    </div>
+                    </div>
+                    </div>
+                   
+                   
+               </div>
+            );
+
+        
+       
     }
 
 
