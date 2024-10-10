85c9d5e15f9994bc50b09ca6c9223e5a146ae3db
Harini S <harinipotter7@gmail.com>
2024-10-10 16:46:40 -0400

    Updates

---
+++ precache-manifest.505f5326db630b2543f44f35ed4813d8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f561e39792d4794650b789db52da9695",
+    "url": "/RestaurantWebsite/confusion/index.html"
+  },
+  {
+    "revision": "563a01e45628f2488dcd",
+    "url": "/RestaurantWebsite/confusion/static/css/2.72fb4ee4.chunk.css"
+  },
+  {
+    "revision": "d3d75d5d28d6c1c306e5",
+    "url": "/RestaurantWebsite/confusion/static/css/main.9c311786.chunk.css"
+  },
+  {
+    "revision": "563a01e45628f2488dcd",
+    "url": "/RestaurantWebsite/confusion/static/js/2.51c73acd.chunk.js"
+  },
+  {
+    "revision": "7d2579c8bc39cbb93e4298ffb99d34c7",
+    "url": "/RestaurantWebsite/confusion/static/js/2.51c73acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d75d5d28d6c1c306e5",
+    "url": "/RestaurantWebsite/confusion/static/js/main.b701807c.chunk.js"
+  },
+  {
+    "revision": "891560a3abf747ae9d66",
+    "url": "/RestaurantWebsite/confusion/static/js/runtime-main.d3af628a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/RestaurantWebsite/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/RestaurantWebsite/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/RestaurantWebsite/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/RestaurantWebsite/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/RestaurantWebsite/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
